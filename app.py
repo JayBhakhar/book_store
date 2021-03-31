5b86510cc264d01f16b66660782eb1998f7bad14
Jay Bhakhar <jaybhakhar@gmail.com>
2021-03-31 13:23:01 +0500

    connect to database

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_pymongo import MongoClient
 
 app = Flask(__name__)
+
+MongoURL = "mongodb+srv://JayBhakhar:jay456789@book-cluster.oec1c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+client = MongoClient(MongoURL).books_datadase.book
 
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    BooksData = []
+    for doc in client.find():
+        BooksData.append({'name': doc['name'],
+                           'pages': doc['pages'],
+                           'book': doc['book'],
+                           '_id': str(doc['_id'])
+                           })
+    return jsonify({'books': BooksData})
 
 
 if __name__ == '__main__':
